11aead91d4e53e4f824e7ba514cb3e2e00e7337a
ripidot <ffi6t5t2fy@gmail.com>
2025-05-07 06:35:34 +0900

    some files and folders
    ディレクトリ構造の変更
    KeyMapLoaderクラスの分割

---
+++ CLI/src/KeyMapLoader.cpp
@@ -0,0 +1,86 @@
+#include "../include/KeyMapLoader.h"
+
+const std::unordered_map<std::string, WORD> KeyMapLoader::vk_map_ = {
+    {"VK_BACK", VK_BACK},
+    {"VK_TAB", VK_TAB},
+    {"VK_RETURN", VK_RETURN},
+    {"VK_SHIFT", VK_SHIFT},
+    {"VK_CONTROL", VK_CONTROL},
+    {"VK_MENU", VK_MENU}, // Alt
+    {"VK_PAUSE", VK_PAUSE},
+    {"VK_CAPITAL", VK_CAPITAL}, // Caps Lock
+    {"VK_ESCAPE", VK_ESCAPE},
+    {"VK_SPACE", VK_SPACE},
+    {"VK_PRIOR", VK_PRIOR}, // Page Up
+    {"VK_NEXT", VK_NEXT},   // Page Down
+    {"VK_END", VK_END},
+    {"VK_HOME", VK_HOME},
+    {"VK_LEFT", VK_LEFT},
+    {"VK_UP", VK_UP},
+    {"VK_RIGHT", VK_RIGHT},
+    {"VK_DOWN", VK_DOWN},
+    {"VK_INSERT", VK_INSERT},
+    {"VK_DELETE", VK_DELETE},
+    {"VK_LWIN", VK_LWIN},
+    {"VK_RWIN", VK_RWIN},
+    {"VK_APPS", VK_APPS},
+    {"VK_NUMPAD0", VK_NUMPAD0},
+    {"VK_NUMPAD1", VK_NUMPAD1},
+    {"VK_NUMPAD2", VK_NUMPAD2},
+    {"VK_NUMPAD3", VK_NUMPAD3},
+    {"VK_NUMPAD4", VK_NUMPAD4},
+    {"VK_NUMPAD5", VK_NUMPAD5},
+    {"VK_NUMPAD6", VK_NUMPAD6},
+    {"VK_NUMPAD7", VK_NUMPAD7},
+    {"VK_NUMPAD8", VK_NUMPAD8},
+    {"VK_NUMPAD9", VK_NUMPAD9},
+    {"VK_MULTIPLY", VK_MULTIPLY},
+    {"VK_ADD", VK_ADD},
+    {"VK_SUBTRACT", VK_SUBTRACT},
+    {"VK_DECIMAL", VK_DECIMAL},
+    {"VK_DIVIDE", VK_DIVIDE},
+    {"VK_F1", VK_F1},
+    {"VK_F2", VK_F2},
+    {"VK_F3", VK_F3},
+    {"VK_F4", VK_F4},
+    {"VK_F5", VK_F5},
+    {"VK_F6", VK_F6},
+    {"VK_F7", VK_F7},
+    {"VK_F8", VK_F8},
+    {"VK_F9", VK_F9},
+    {"VK_F10", VK_F10},
+    {"VK_F11", VK_F11},
+    {"VK_F12", VK_F12},
+    {"VK_LSHIFT", VK_LSHIFT},
+    {"VK_RSHIFT", VK_RSHIFT},
+    {"VK_LCONTROL", VK_LCONTROL},
+    {"VK_RCONTROL", VK_RCONTROL},
+    {"VK_NONCONVERT", VK_NONCONVERT},
+    {"VK_VOLUME_DOWN", VK_VOLUME_DOWN},
+    {"VK_VOLUME_UP", VK_VOLUME_UP},
+    {"VK_MEDIA_PLAY_PAUSE", VK_MEDIA_PLAY_PAUSE},
+    {"VK_ZENHANKAKU", 243}
+};
+
+KeyMapLoader::KeyMapLoader(std::string fileurl) : fileurl_(fileurl) {}
+KeyMapLoader::KeyMapLoader() : fileurl_() {}
+
+WORD KeyMapLoader::key_string_to_vk(const std::string& key_name) {
+
+    // "VK_"で始まる名前ならテーブル検索
+    auto it = vk_map_.find(key_name);
+    if (it != vk_map_.end()) {
+        return it->second;
+    }
+
+    // アルファベット単文字（大文字小文字問わず）
+    if (key_name.size() == 1) {
+        char c = key_name[0];
+        if ('a' <= c && c <= 'z') c -= 32; // 小文字→大文字変換
+        if ('A' <= c && c <= 'Z') return static_cast<WORD>(c);
+        if ('0' <= c && c <= '9') return static_cast<WORD>(c);
+    }
+
+    // それ以外は失敗
+    return 0;
+}
