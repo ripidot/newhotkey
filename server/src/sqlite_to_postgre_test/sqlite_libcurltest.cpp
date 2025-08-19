d089a2825f8931e67521543f0b3545178b75328b
ripidot <ffi6t5t2fy@gmail.com>
2025-08-19 12:08:18 +0900

    update: apiの変更
    キャッシュのsqlite, エラーログのsqliteとの連携のため

--- server/src/sqlite_to_postgre_test/sqlite_libcurltest.cpp
+++ server/src/sqlite_to_postgre_test/sqlite_libcurltest.cpp
@@ -22,7 +22,10 @@ struct KeyLog {
     std::string process_name;
     int user_id;
 };
-
+struct FailedLog {
+    int id;
+    std::string reason;
+};
 std::vector<KeyLog> fetch_unsent_logs(sqlite3* db) {
     std::vector<KeyLog> logs;
     const char* sql = "SELECT id, session_id, sequence_id, timestamp, key, modifiers, window_title, process_name, user_id FROM keylogs WHERE sent_flag = 0 LIMIT 3;";
@@ -142,6 +145,67 @@ bool delete_sent_logs(sqlite3* db, const std::vector<int>& success_ids) {
     sqlite3_finalize(stmt);
     return true;
 }
+
+void from_json(const nlohmann::json& j, FailedLog& f) {
+    j.at("id").get_to(f.id);
+    j.at("reason").get_to(f.reason);
+}
+
+bool escape_invalid_logs(std::vector<KeyLog>& logs, const std::vector<FailedLog>& failed) {
+    std::string DB_URL = "keylog_invalid_column.db";
+    sqlite3* db;
+    if (sqlite3_open(DB_URL.c_str(), &db) != SQLITE_OK) {
+        std::cerr << "Cannot open database" << std::endl;
+        return 0;
+    }
+    sqlite3_stmt* stmt = nullptr;
+    const char* sql = "INSERT INTO keylogs"
+    "(session_id, sequence_id, timestamp, key, modifiers, window_title, process_name, user_id)"
+    "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+
+    // トランザクション処理
+    char* errmsg = nullptr;
+    if (sqlite3_exec(db, "BEGIN TRANSACTION;", nullptr, nullptr, &errmsg) != SQLITE_OK) {
+        std::cerr << "Failed to begin transaction: " << errmsg << std::endl;
+        sqlite3_free(errmsg);
+        sqlite3_finalize(stmt);
+        return false;
+    }
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "SQL prepare error: " << sqlite3_errmsg(db) << std::endl;
+        return false;
+    }
+    for (FailedLog flog : failed) {
+        std::cout << "flog.id : " << flog.id << std::endl;
+        sqlite3_reset(stmt);
+        for (auto log : logs) if (log.id == flog.id){
+            sqlite3_bind_text(stmt, 1, log.session_id.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_int(stmt,  2, log.sequence_id);
+            sqlite3_bind_text(stmt, 3, log.timestamp.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 4, log.key.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 5, log.modifiers.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 6, log.window_title.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 7, log.process_name.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_int(stmt,  8, log.user_id);
+            if (sqlite3_step(stmt) != SQLITE_DONE) {
+                std::cerr << "Insert failed from ID=" << flog.id << ": " << sqlite3_errmsg(db) << std::endl;
+                sqlite3_exec(db, "ROLLBACK;", nullptr, nullptr, nullptr);
+                sqlite3_finalize(stmt);
+                return false;
+            }
+        }
+
+    }
+    sqlite3_finalize(stmt);
+
+    if (sqlite3_exec(db, "COMMIT;", nullptr, nullptr, &errmsg) != SQLITE_OK) {
+        std::cerr << "Failed to commit: " << errmsg << std::endl;
+        sqlite3_free(errmsg);
+        return false;
+    }
+    return true;
+}
 int main() {
     std::string DB_URL = "keylog_bac_2_10column.db";
     sqlite3* db;
@@ -169,6 +233,9 @@ int main() {
         std::vector<int> success_ids =
             json_resp.value("success_ids", nlohmann::json::array()).get<std::vector<int>>();
 
+        std::vector<FailedLog> failed =
+            json_resp.value("failed", nlohmann::json::array()).get<std::vector<FailedLog>>();
+
         auto success_len = json_resp["success_count"];
         std::cout << "http_code : " << http_code << std::endl;
         std::cout << success_ids.size() << std::endl;
@@ -178,6 +245,7 @@ int main() {
             std::cout << "Success" << std::endl;
         } else if (http_code == 400) { // 部分成功
             delete_sent_logs(db, success_ids);
+            escape_invalid_logs(logs, failed);
             std::cout << "Client error (bad request, duplicate, etc.)" << std::endl;
         } else if (http_code == 500) { // サーバーエラー
             std::cout << "Server error" << std::endl;
