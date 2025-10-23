cbc79ed6334f2e323366df2e2387f1b15109d497
ripidot <ffi6t5t2fy@gmail.com>
2025-10-24 04:41:01 +0900

    update: xss対策用のnext.config.ts変更

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,13 +1,41 @@
+// next.config.ts
+import type { NextConfig } from "next";
 
-import type { Configuration as WebpackDevMiddlewareConfig } from 'webpack-dev-middleware'
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  connect-src 'self' https://newhotkey.onrender.com;
+  font-src 'self';
+  frame-src 'none';
+`;
 
-module.exports = {
-  webpack: (config : WebpackDevMiddlewareConfig) => {
-    config.watchOptions = {
-      ignored: ['**/node_modules'],
-      aggregateTimeout: 300,
-      poll: 5000
-    }
-    return config;
-  }
-}
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+];
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
